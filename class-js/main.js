3bc2009175ee3a7076d9d76390832c7ce6b0f1b2
RammSanker <109334253+rammsanker54@users.noreply.github.com>
2022-10-14 11:12:03 +0530

    Add files via upload

---
+++ class-js/main.js
@@ -0,0 +1,13 @@
+
+export class personal{
+    constructor(){
+    this.name="Rammsanker"
+    this.age="24"
+    this.state="TamilNadu"
+}
+}
+console.log(personal,typeof personal)
+
+// console.log(age.personal,typeof personal)
+
+
